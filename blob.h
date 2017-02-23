b32e2a130655c3baf5c2b0a519e9ab90aab58564
Paolo Torres <paolo-torres@hotmail.com>
2017-02-22 22:58:17 -0500

    Add files via upload

---
+++ blob.h
@@ -0,0 +1,36 @@
+#ifndef MY_BLOB
+#define MY_BLOB
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+class Blob {
+public:
+
+	vector<Point> currentContour;
+
+	Rect currentBoundingRect;
+
+	vector<Point> centerPositions;
+
+	double CurrentDiagonalSize;
+	double CurrentAspectRatio;
+
+	bool CurrentMatchFoundOrNewBlob;
+
+	bool StillBeingTracked;
+
+	int NumOfConsecutiveFramesWithoutAMatch;
+
+	Point predictedNextPosition;
+
+	Blob(vector<Point> _contour);
+	void predictNextPosition(void);
+
+};
+
+#endif
