fb29d24382d1ff159da8e478138b68f1d7e912d2
Paolo Torres <paolo-torres@hotmail.com>
2017-03-02 07:51:10 -0500

    Update blob.h

--- blob.h
+++ blob.h
@@ -1,9 +1,9 @@
 #ifndef MY_BLOB
 #define MY_BLOB
 
-#include<opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
